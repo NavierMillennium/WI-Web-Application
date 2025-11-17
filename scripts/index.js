10b4f90be1f58cd6f7060d942d2115d3d2a3ee20
kskop <kskop@agh.edu.pl>
2025-11-17 11:45:12 +0100

    Adding slider

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,32 @@ function setCookie(name, value, days) {
 }
 
 setCookie("username", "John Doe", 7);
+
+
+const slides = document.querySelectorAll(".slider-data");
+let index = 0;
+
+function showSlide(i) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[i].classList.add("active");
+}
+
+
+document.getElementById("next").addEventListener("click", () => {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+});
+
+document.getElementById("prev").addEventListener("click", () => {
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide(index);
+});
+
+
+showSlide(index);
+
+
+setInterval(() => {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+}, 5000);
